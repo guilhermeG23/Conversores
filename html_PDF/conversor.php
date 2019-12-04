1f622ed79f2cf1e59d4c5c287fb3724c4a2a0046
guilhermeG23 <guilhermebrechot@gmail.com>
2019-12-04 11:03:44 -0300

    Ajustada a linha de limpeza do diretorio de manipulacao dos arquivos do PDF

--- html_PDF/conversor.php
+++ html_PDF/conversor.php
@@ -7,7 +7,7 @@ if (isset($_FILES['arquivo'])) {
 	$arquivo_input = '/var/www/html/html_PDF/trabalho/'.$arquivo;
 	move_uploaded_file($_FILES['arquivo']['tmp_name'], $arquivo_input);
 	shell_exec("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/var/www/html/html_PDF/arquivos/$arquivo $arquivo_input");
-	#shell_exec("rm -rf ./trabalho/*.pdf");
+	shell_exec("rm -rf ./trabalho/*.pdf");
 	header("Location: index.php");
 	die();
 } else {
