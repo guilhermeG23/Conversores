2229e11f7fc311e4fb5505b5cc9f4ac74c56b09a
guilhermeG23 <guilhermebrechot@gmail.com>
2019-12-04 10:50:19 -0300

    Arrumando o caminho dos diretorios

--- html_DVR/conversor.php
+++ html_DVR/conversor.php
@@ -5,9 +5,9 @@ if (isset($_FILES['arquivo'])) {
 	$arquivo = preg_replace('/[^A-Za-z0-9]/', '_', $arquivo);
 	$arquivo_extensao_mp4 = $arquivo . '.mp4';
 	$arquivo_extensao_dav = $arquivo . '.dav';
-	$arquivo_input = '/var/www/html/trabalho/'.$arquivo_extensao_dav;
+	$arquivo_input = '/var/www/html/html_DVR/trabalho/'.$arquivo_extensao_dav;
 	move_uploaded_file($_FILES['arquivo']['tmp_name'], $arquivo_input);
-	shell_exec("ffmpeg -y -i $arquivo_input -vcodec libx264 -crf 24 -filter:v 'setpts=1*PTS' /var/www/html/arquivos/$arquivo_extensao_mp4");
+	shell_exec("ffmpeg -y -i $arquivo_input -vcodec libx264 -crf 24 -filter:v 'setpts=1*PTS' /var/www/html/html_DVR/arquivos/$arquivo_extensao_mp4");
 	shell_exec("rm -rf ./trabalho/*.dav");
 	header("Location: index.php");
 	die();
--- html_PDF/conversor.php
+++ html_PDF/conversor.php
@@ -4,10 +4,10 @@ if (isset($_FILES['arquivo'])) {
 	$arquivo = $_FILES['arquivo']['name'];
 	$arquivo = preg_replace('/[^A-Za-z0-9]/', '_', $arquivo);
 	$arquivo = $arquivo . '.pdf';
-	$arquivo_input = '/var/www/html/trabalho/'.$arquivo;
+	$arquivo_input = '/var/www/html/html_PDF/trabalho/'.$arquivo;
 	move_uploaded_file($_FILES['arquivo']['tmp_name'], $arquivo_input);
-	shell_exec("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/var/www/html/arquivos/$arquivo $arquivo_input");
-	shell_exec("rm -rf ./trabalho/*.pdf");
+	shell_exec("gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/screen -sColorConversionStrategy=Gray -dProcessColorModel=/DeviceGray -dNOPAUSE -dQUIET -dBATCH -sOutputFile=/var/www/html/html_PDF/arquivos/$arquivo $arquivo_input");
+	#shell_exec("rm -rf ./trabalho/*.pdf");
 	header("Location: index.php");
 	die();
 } else {
